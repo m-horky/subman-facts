6c3f0bdd8b4ce231df8b59b322854f947b69b74e
Matyas Horky <mhorky@redhat.com>
2023-06-22 15:55:08 +0200

    Network collector for 'net.*' facts
    It is not (yet) 1:1 with subscription-manager, it does miss broadcast
    addresses, for example.

--- pkg/collector/network.go
+++ pkg/collector/network.go
@@ -1,6 +1,7 @@
 package collector
 
 import (
+	"fmt"
 	"github.com/Showmax/go-fqdn"
 	"net"
 	"net/netip"
@@ -8,6 +9,22 @@ import (
 	"os/exec"
 	"strings"
 )
+
+type IPAddressType int
+
+const (
+	IPUnspecifiedAddress IPAddressType = iota
+	IPGlobalAddress
+	IPLocalAddress
+	IPHostAddress
+)
+
+type IPWithMask struct {
+	Address netip.Addr
+	Mask    string
+	Version int
+	Type    IPAddressType
+}
 
 type NetworkCollector struct {
 	data map[string]string
@@ -55,6 +72,11 @@ func (c *NetworkCollector) collect() error {
 		// TODO Log the error
 	}
 
+	err = c.collectInterfaces()
+	if err != nil {
+		// TODO Log the error
+	}
+
 	return nil
 }
 
@@ -86,9 +108,44 @@ func (c *NetworkCollector) collectFQDN_shell() error {
 	return nil
 }
 
+// getInterfaceAddresses inspect an interface and returns its v4 and v6 addresses.
+func getInterfaceAddresses(iface net.Interface) ([]IPWithMask, error) {
+	var addresses []IPWithMask
+
+	addrs, _ := iface.Addrs()
+	for _, addr := range addrs {
+		addrWithoutMask, maskStr, ok := strings.Cut(addr.String(), "/")
+		if !ok {
+			// TODO Log the error
+		}
+		address := netip.MustParseAddr(addrWithoutMask)
+
+		// Categorize the address
+		var addressType IPAddressType
+		if address.IsGlobalUnicast() {
+			addressType = IPGlobalAddress
+		} else if address.IsLoopback() {
+			addressType = IPHostAddress
+		} else if address.IsLinkLocalUnicast() || address.IsLinkLocalMulticast() {
+			addressType = IPLocalAddress
+		}
+
+		if address.Is4() {
+			addresses = append(addresses, IPWithMask{address, maskStr, 4, addressType})
+		} else {
+			addresses = append(addresses, IPWithMask{address, maskStr, 6, addressType})
+		}
+	}
+
+	return addresses, nil
+}
+
+// collectAddresses gathers 'network.ipv4_address' and 'network.ipv6_address' facts.
 func (c *NetworkCollector) collectAddresses() error {
-	var ipv4s = []string{}
-	var ipv6s = []string{}
+	var v4Addresses []string
+	var v4Masks []string
+	var v6Addresses []string
+	var v6Masks []string
 
 	ifaces, _ := net.Interfaces()
 	for _, iface := range ifaces {
@@ -97,24 +154,101 @@ func (c *NetworkCollector) collectAddresses() error {
 			continue
 		}
 
-		addrs, _ := iface.Addrs()
-		for _, addr := range addrs {
-			addrWithoutMask := strings.Split(addr.String(), "/")[0]
-			address := netip.MustParseAddr(addrWithoutMask)
+		addresses, err := getInterfaceAddresses(iface)
+		if err != nil {
+			return err
+		}
 
-			if address.Is4() {
-				ipv4s = append(ipv4s, address.String())
+		for _, address := range addresses {
+			if address.Version == 4 {
+				v4Addresses = append(v4Addresses, address.Address.String())
+				v4Masks = append(v4Masks, address.Mask)
 			} else {
 				// Do not include link local addresses
-				if address.IsLinkLocalUnicast() || address.IsLinkLocalMulticast() {
+				if address.Type == IPLocalAddress {
 					continue
 				}
-				ipv6s = append(ipv6s, address.String())
+				v6Addresses = append(v6Addresses, address.Address.String())
+				v6Masks = append(v6Masks, address.Mask)
 			}
 		}
+
 	}
 
-	c.data["network.ipv4_address"] = strings.Join(ipv4s, ", ")
-	c.data["network.ipv6_address"] = strings.Join(ipv6s, ", ")
+	c.data["network.ipv4_address"] = strings.Join(v4Addresses, ", ")
+	c.data["network.ipv6_address"] = strings.Join(v6Addresses, ", ")
+	return nil
+}
+
+// collectInterfaces gathers all 'net.' facts.
+func (c *NetworkCollector) collectInterfaces() error {
+	ifaces, _ := net.Interfaces()
+	for _, iface := range ifaces {
+		prefix := fmt.Sprintf("net.interface.%s", iface.Name)
+		addresses, err := getInterfaceAddresses(iface)
+		if err != nil {
+			// TODO Log the error
+		}
+
+		var v4a []string
+		var v4m []string
+		var v6ga []string
+		var v6gm []string
+		var v6la []string
+		var v6lm []string
+		var v6ha []string
+		var v6hm []string
+
+		for _, address := range addresses {
+			if address.Version == 4 {
+				v4a = append(v4a, address.Address.String())
+				v4m = append(v4m, address.Mask)
+			} else if address.Type == IPGlobalAddress {
+				v6ga = append(v6ga, address.Address.String())
+				v6gm = append(v6gm, address.Mask)
+			} else if address.Type == IPLocalAddress {
+				v6la = append(v6la, address.Address.String())
+				v6lm = append(v6lm, address.Mask)
+			} else if address.Type == IPHostAddress {
+				v6ha = append(v6ha, address.Address.String())
+				v6hm = append(v6hm, address.Mask)
+			}
+		}
+
+		if len(v4a) > 0 {
+			c.data[fmt.Sprintf("%s.ipv4_address", prefix)] = v4a[0]
+			c.data[fmt.Sprintf("%s.ipv4_address_list", prefix)] = strings.Join(v4a, ", ")
+		}
+		if len(v4m) > 0 {
+			c.data[fmt.Sprintf("%s.ipv4_netmask", prefix)] = v4m[0]
+			c.data[fmt.Sprintf("%s.ipv4_netmask_list", prefix)] = strings.Join(v4m, ", ")
+		}
+		if len(v6ga) > 0 {
+			c.data[fmt.Sprintf("%s.ipv6_address.global", prefix)] = v6ga[0]
+			c.data[fmt.Sprintf("%s.ipv6_address.global_list", prefix)] = strings.Join(v6ga, ", ")
+		}
+		if len(v6gm) > 0 {
+			c.data[fmt.Sprintf("%s.ipv6_netmask.global", prefix)] = v6gm[0]
+			c.data[fmt.Sprintf("%s.ipv6_netmask.global_list", prefix)] = strings.Join(v6gm, ", ")
+		}
+		if len(v6la) > 0 {
+			c.data[fmt.Sprintf("%s.ipv6_address.link", prefix)] = v6la[0]
+			c.data[fmt.Sprintf("%s.ipv6_address.link_list", prefix)] = strings.Join(v6la, ", ")
+		}
+		if len(v6lm) > 0 {
+			c.data[fmt.Sprintf("%s.ipv6_netmask.link", prefix)] = v6lm[0]
+			c.data[fmt.Sprintf("%s.ipv6_netmask.link_list", prefix)] = strings.Join(v6lm, ", ")
+		}
+		if len(v6ha) > 0 {
+			c.data[fmt.Sprintf("%s.ipv6_address.host", prefix)] = v6ha[0]
+			c.data[fmt.Sprintf("%s.ipv6_address.host_list", prefix)] = strings.Join(v6ha, ", ")
+		}
+		if len(v6hm) > 0 {
+			c.data[fmt.Sprintf("%s.ipv6_netmask.host", prefix)] = v6hm[0]
+			c.data[fmt.Sprintf("%s.ipv6_netmask.host_list", prefix)] = strings.Join(v6hm, ", ")
+		}
+
+	}
+
 	return nil
 }
