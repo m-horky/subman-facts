54578e0a5cfd6db41cf3cd7011d63356cd8f6b78
Matyas Horky <mhorky@redhat.com>
2023-07-07 11:15:36 +0200

    system: Certificate version collector

--- pkg/collector/system.go
+++ pkg/collector/system.go
@@ -1,5 +1,7 @@
 package collector
 
+import "git.sr.ht/~spc/go-log"
+
 type SystemFacts struct {
 	CollectedFacts     `json:"-"`
 	CertificateVersion string `json:"certificate_version"`
@@ -27,6 +29,8 @@ func (c *SystemCollector) GetData(rescan bool) (CollectedFacts, error) {
 
 // collect starts the actual fact collection. It is usually invoked by GetData.
 func (c *SystemCollector) collect() error {
+	_ = c.collectCertificateVersion()
+	_ = c.collectDefaultLocale()
 	return nil
 }
 
@@ -35,10 +39,13 @@ func (c *SystemCollector) collectCertificateVersion() error {
 	// TODO Find out which certificate it should be (/etc/pki/entitlement/ or
 	//  /etc/pki/consumer/)
 	// see src/rhsmlib/facts/collector.py::StaticFactsCollector
+	c.data.CertificateVersion = "3.2"
+	log.Warn("System's certificate version fact is hardcoded, consider loading it dynamically.")
 	return nil
 }
 
 func (c *SystemCollector) collectDefaultLocale() error {
 	// see src/rhsmlib/facts/host_collector.py
+	log.Debug("Locale fact collection is not implemented.")
 	return nil
 }
